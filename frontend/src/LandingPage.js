9ace62233a9f5ffcaf8573d69c14c34df1f8ad3f
naluconcepcion <naluconcepcion@gmail.com>
2019-05-26 13:17:45 -0400

    updating files

---
+++ frontend/src/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
+
+function LandingPage() {
+  return (
+
+  );
+}
+
+export default LandingPage;
